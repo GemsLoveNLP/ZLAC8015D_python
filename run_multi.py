6b7139429d0a47e4c885f898cf279174d78169ff
GemsLoveNLP <6438169421@student.chula.ac.th>
2024-11-29 11:01:56 +0700

    add: tick report

--- run_multi.py
+++ run_multi.py
@@ -3,11 +3,12 @@ import keyboard
 import time
 import os
 
-RPM = 15
+RPM = 250
 RPM_DICT = {"w":[-RPM,RPM],
             "a":[RPM,RPM],
             "s":[RPM,-RPM],
-            "d":[-RPM,-RPM]}
+            "d":[-RPM,-RPM],
+            "x":[0,0]}
 
 
 def connect_motors():
@@ -29,7 +30,9 @@ def connect_motors():
         motors.set_mode(3)
         motors.enable_motor()
 
-        motors_lst.append((port,motors))
+        l_tick_i, r_tick_i = motors.get_wheels_tick()
+
+        motors_lst.append((port,motors,l_tick_i,r_tick_i))
 
     return motors_lst
 
@@ -37,7 +40,7 @@ def main():
     motors_lst = connect_motors()
     try:
         last_time = time.time()
-        for port, motors in motors_lst:
+        for port, motors, l_tick_i,r_tick_i in motors_lst:
             motors.set_rpm(0, 0)
         while True:
             # get keyboard input
@@ -45,30 +48,37 @@ def main():
 
             if key in RPM_DICT:
                 cmds = RPM_DICT[key]
-                for port, motors in motors_lst:
+                for port, motors, l_tick_i,r_tick_i in motors_lst:
                     motors.set_rpm(cmds[0], cmds[1])
             else:
                 print("Turning off motors and exiting...")
-                for port, motors in motors_lst:
+                for port, motors, l_tick_i,r_tick_i in motors_lst:
                     motors.disable_motor()
                 return
 
             # report speed
-            for port, motors in motors_lst:
+            for port, motors, l_tick_i,r_tick_i in motors_lst:
                 rpmL, rpmR = motors.get_rpm()
+                # TODO: add a check. If the speed flips by above some value, regulate it.
+                l_tick, r_tick = motors.get_wheels_tick()
+                l_tick -= l_tick_i
+                l_tick *= -1 # ? this is to make sure that 
+                r_tick -= r_tick_i
                 period = time.time() - last_time
                 print("Port: {}, Period: {:.4f} rpmL: {:.1f} | rpmR: {:.1f}".format(port, period, rpmL, rpmR))
+                # motors.set_rpm(cmds[0],cmds[1])
+                print("period: {:.4f} l_tick: {:.1f} | r_tick: {:.1f}".format(period,l_tick,r_tick))
 
     except KeyboardInterrupt:
         # Handle keyboard interrupt (Ctrl+C) gracefully
         print("Keyboard interrupt detected. Disabling motors...")
-        for port, motors in motors_lst:
+        for port, motors, l_tick_i,r_tick_i in motors_lst:
             motors.disable_motor()
 
     except Exception as e:
         # Call disable_motor() in case of error
         print(f"An error occurred: {e}")
-        for port, motors in motors_lst:
+        for port, motors, l_tick_i,r_tick_i in motors_lst:
             motors.disable_motor()
 
 if __name__ == "__main__":
